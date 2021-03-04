1e27dcf7ff71491f9de5baca4fe890ebabd8ba73
KokoDoko <erik.katerborg@gmail.com>
2021-03-04 13:07:08 +0100

    Added finished 🐈  code

---
+++ week3/hands-on/finished/js/app.js
@@ -0,0 +1,35 @@
+const cardsContainer = document.querySelector("#cards");
+
+const cats = [
+    { name: "Dr. Fluffenstein", image: "./images/cat1.jpg", movie: "Avengers" },
+    { name: "Whiskers", image: "./images/cat2.jpg" },
+    { name: "Goose", image: "./images/cat3.jpg" },
+    { name: "Whiskers", image: "./images/cat4.jpg" }
+];
+
+function addCard(cat) {
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("card");
+    cardsContainer.appendChild(cardDiv);
+
+    const img = document.createElement("img");
+    img.src = cat.image;
+    cardDiv.appendChild(img);
+
+    const nameDiv = document.createElement("div");
+    nameDiv.innerText = cat.name;
+    cardDiv.appendChild(nameDiv);
+
+    if (cat.movie) {
+        nameDiv.innerText = `${cat.name} movie: ${cat.movie}`;
+    }
+}
+
+function addCards() {
+    for (let cat of cats) {
+        addCard(cat);
+    }
+}
+
+addCards();
+addCards();
